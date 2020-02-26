8650a4af2193705ffbe33bc7c3dcec36c57f28b4
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-26 16:20:19 -0600

    added middleware for private route to handle if theres a token

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const Users = require('../users/users-model')
 
 const router = express.Router();
 
 router.get('/', (req, res) =>
 {
-    res.json({message: 'hello from users'});
+   Users.findAllUsers()
+   .then(users =>
+    {
+        res.status(200).json(users);
+    })
+   .catch(error =>
+    {
+        res.status(500).json({error: 'Unable to get all users'});
+    })
 })
 
 module.exports = router;
