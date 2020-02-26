71ad15ff23e97cf8216ba00583a811bbb44f0d14
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-26 14:53:58 -0600

    api router is created and imported works still need to add other endpoints in there

---
+++ api/apiRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/register', (req, res) =>
+{
+    res.json({message: 'hello from register'});
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 require('dotenv').config();
+const apiRouter = require('./api/apiRouter');
 
 const server = express();
 
 server.use(helmet())
 server.use(express.json());
+
+server.use('/api', apiRouter);
 
 server.get('/', (req, res) =>
 {
