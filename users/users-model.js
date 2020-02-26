060e61352e22d6d343ad65998c2c9349fb3a121a
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-26 15:30:47 -0600

    created skeleton for users-model

---
+++ users/users-model.js
@@ -0,0 +1,22 @@
+const database = require('../data/dbConfig');
+
+function addUser(user)
+{
+
+}
+
+function findUser(user)
+{
+
+}
+
+function findAllUsers()
+{
+
+}
+
+module.exports = {
+    addUser,
+    findUser,
+    findAllUsers
+}
